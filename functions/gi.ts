3f4e830cd1d438e1948b04539d766494aa836a62
dvaJi <francisco_p@outlook.com>
2023-10-26 11:19:00 -0300

    fix response

--- functions/gi.ts
+++ functions/gi.ts
@@ -7,13 +7,16 @@ export default async (req: Request, context: Context) => {
   const name = new URL(req.url).searchParams.get("name") ?? "Stranger";
   const characters = await giData.characters();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
+  return new Response(
+    JSON.stringify({
       message: `Hello, ${name}!`,
       characters: characters,
     }),
-  };
+    {
+      status: 200,
+      headers: { "content-type": "application/json" },
+    }
+  );
 };
 
 export const config: Config = {
